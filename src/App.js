5ccd1b250a4838e259a950a9451b30fa15d11d74
Rainbooboo <omoneferuemu@gmail.com>
2024-09-27 04:04:38 +0100

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './App.css';
-
+import Navbar from './components/Navbar/navbar';
+import Main from './components/Main/main';
+import Footer from './components/Footer/footer';
+import Home from './components/Home/home';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar/>
+    {/* <Main/>
+    <Footer/>
+    <Home/> */}
+    </>
   )
 }
 
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState } from 'react';
+import './navbar.css';
+import { MdTravelExplore } from "react-icons/md";
+import { IoIosCloseCircle } from "react-icons/io";
+import { TbGridDots } from "react-icons/tb";
+
+const Navbar = () => {
+    const  [active, setaActive] = useState('navBar');
+
+    //Toggle navbar function
+    const showNavbar = ()=>{
+        setaActive('navBar activeNavbar')
+    }
+    //Close navbar function
+    const closeNavbar = ()=>{
+        setaActive('navBar')
+    }
+
+  return (
+    <section className="navBarSection">
+        <header className="header flex">
+            <div className="logoDiv">
+                <a href="/#" className="logo flex">
+                    <h1 className="/#"> <MdTravelExplore className="icon"/>Travel.</h1>
+                </a>
+            </div>
+            <div className={active}>
+                <ul className="navLists flex">
+                    <li className="navItem">
+                        <a href="/#" className="navLink">Home</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/#" className="navLink">Packages</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/#" className="navLink">Shop</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/#" className="navLink">Amount</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/#" className="navLink">Pages</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/#" className="navLink">News</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="//#" className="navLink">Contact</a>
+                    </li>
+                    <button className="btn">
+                        <a href="/#">BOOK NOW</a>
+                    </button>
+                </ul>
+                <div onClick={closeNavbar} className="closeNavbar">
+                    <IoIosCloseCircle className="icon"/>
+                </div>
+            </div>
+            <div onClick={showNavbar} className="toggleNavbar">
+                <TbGridDots className='icon'/>
+            </div>
+        </header>
+    </section>
+  )
+}
+
+export default Navbar
